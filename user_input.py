90d329f5ab269bc776eef6e69a0d20a3768ba702
Júnior Baum <juniorsbaum@hotmail.com>
2023-01-21 01:21:39 -0300

    feat: input lê multiplos cnpjs e imprime um a um. devem estar separados por virgula

--- user_input.py
+++ user_input.py
@@ -4,13 +4,13 @@ from db import Processes
 
 class InputOptions:
     def user_input(self):
-        cnpj = input("Digite o CNPJ: ")
-        data = get_cnpj(cnpj)
-        print("Dados obtidos com sucesso!")
-        decision = input("Voce quer inserir essa info no banco de dados?? s/n: ")
-        if decision == 's' or decision == 'S':
-            Processes.insert_one(data)
-            print("Dados inseridos no banco de dados.")
-        else:
-            print("Dados nao inseridos no banco de dados.")
-
+        cnpjs = input("Digite os CNPJs separados por vírgula: ")
+        cnpjs = cnpjs.split(",")
+        for cnpj in cnpjs:
+            try:
+                data = get_cnpj(cnpj)
+                print(f"Dados obtidos com sucesso para o CNPJ {cnpj}!")
+                Processes.insert_one(data)
+                print(f"Dados inseridos no banco de dados para o CNPJ {cnpj}.")
+            except Exception as e:
+                print(f"Erro ao obter dados para o CNPJ {cnpj}: {e}")
